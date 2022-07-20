be4c837e787625d6a81b3ad30ddb200c64bb8ea3
Karn Tiwari <karntiwari5353@gmail.com>
2022-07-20 12:00:12 +0530

    lasdhjf

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 let video = document.querySelector('video');
+let recordBtnCont = document.querySelector('.record-btn-cont');
+let recordBtn = document.querySelector('.record-btn');
+let captureBtnCont = document.querySelector('.capture-btn-cont');
+let captureBtn = document.querySelector('.capture-btn');
+let recordFlag = false;
+
+let recorder;
+let chunks = [];// media data in chunks
 
 let constraints = {
     audio: true,
@@ -9,6 +17,44 @@ let constraints = {
 // userMedia -> mediadevices k ander ki ek functionality hai jisme wo user se access mangega
 
 navigator.mediaDevices.getUserMedia(constraints).then((stream) =>{
-video.
+    video.srcObject = stream;
+    recorder = new MediaRecorder(stream);
+    recorder.addEventListener("start",(e)=>{
+        chunks = [];
+    })
+    recorder.addEventListener("dataavailable",(e)=>{
+        chunks.push(e.data);
+    })
+    recorder.addEventListener=("stop", (e) => {// jab bhi  main stop pe click karunga to chunke k form me video download ho jaye aur url generate ho jaye aur ek file me store ho jaye
+        //conversion of media chunks int video mp4
+
+        let blob = new Blob(chunks,{type:"video/mp4"});
+        let videoURL = URL.createObjectURL(blob);// isko through url mill jati hai
+        
+        let a = document.createElement('a');
+        a.href = videoURL;
+        a.download = 'video.mp4';
+        a.click();
+    })
+        
+})
+// mediastream -> this interface returns a stream of media content. and it consist of several tracks such as video or audio
+
+
+recordBtnCont.addEventListener('click', (e) => {
+    if(!recorder)return;
+    recordFlag = !recordFlag;
+    if(recordFlag){
+        recorder.start();
+        recordBtn.classList.add("scale-record")
+    }
+    else{
+        recorder.stop();
+        recordBtn.classList.remove("scale-record");
+    }
 })
 
+
+function startTimer(){
+    setInterval
+}
