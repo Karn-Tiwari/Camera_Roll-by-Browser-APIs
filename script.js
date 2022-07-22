9d23c0009aa82705ba1b97f727d3d473a4b32af6
Karn Tiwari <karntiwari5353@gmail.com>
2022-07-22 12:00:12 +0530

    lasdhjf

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-let video = document.querySelector('video');
+// let video = document.querySelector('video');
 
-let constraints = {
-    audio: true,
-    video: true
-}
-//navigator -> global object hai aur browser ki information deta hai
-// mediaDevices -> iska use hum hardware jaise camera aur microphone ko connect karne k liye karte hai
-// userMedia -> mediadevices k ander ki ek functionality hai jisme wo user se access mangega
+// let constraints = {
+//     audio: true,
+//     video: true
+// }
+// //navigator -> global object hai aur browser ki information deta hai
+// // mediaDevices -> iska use hum hardware jaise camera aur microphone ko connect karne k liye karte hai
+// // userMedia -> mediadevices k ander ki ek functionality hai jisme wo user se access mangega
 
-navigator.mediaDevices.getUserMedia(constraints).then((stream) =>{
-video.
-})
+// navigator.mediaDevices.getUserMedia(constraints).then((stream) =>{
+// video.
+// })
 
